87bcb504fa34b1980017c41ff08566aba22c2161
Darren L <darren.lou@gmail.com>
2022-11-09 15:19:32 -0800

    remove console log in toggleActive

--- 05 - Flex Panel Gallery/index.js
+++ 05 - Flex Panel Gallery/index.js
@@ -1,19 +1,18 @@
 const panels = document.querySelectorAll('.panel')
 
+
+
 function toggleOpen() {
     this.classList.toggle('open')
 }
 
 function toggleActive (e) {
-    console.log(e.propertyName)
     if (e.propertyName.includes('flex')){
         this.classList.toggle('open-active')
     }
 }
 
 panels.forEach(panel => {
-    // panel.addEventListener('mouseover', toggleOpen)
-    // panel.addEventListener('mouseleave', mouseleaveClose)
     panel.addEventListener('click', toggleOpen)
     panel.addEventListener('transitionend', toggleActive)
 }
