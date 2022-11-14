0158d57aa9c43fb550427b97837eefa1ddc15752
Darren Lou <darren.lou@gmail.com>
2022-11-13 19:06:11 -0800

    complete

--- 08 - Fun with HTML5 Canvas/index.js
+++ 08 - Fun with HTML5 Canvas/index.js
@@ -3,27 +3,31 @@ const ctx = canvas.getContext('2d')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-ctx.strokeStyle = '#BADA55'
-ctx.lineJoin = 'round'
-ctx.lineCap = 'round'
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.globalCompositeOperation = 'difference'
+
 
 let isDrawing = false
 let lastX = 0
 let lastY = 0
+let [hue, width] = [0,0];
 
 function draw(e) {
     if (!isDrawing) return;
-
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+    ctx.lineWidth = width;
     ctx.beginPath();
-    // ctx.arc(e.clientX, e.clientY, 10, 0, 2 * Math.PI, false);
-    // ctx.fillStyle = 'orange';
-    // ctx.fill();
     ctx.moveTo(lastX, lastY);
     ctx.lineTo(e.offsetX, e.offsetY);
     ctx.stroke();
     [lastX, lastY] = [e.offsetX, e.offsetY];
-    // lastX = e.offsetX 
-    // lastY = e.offsetY
+    hue >= 360 ? hue = 0 : hue++;
+    width >= 500 ? width = 0 : width++;
+    console.log(ctx.lineWidth, hue, width)
+    // ctx.arc(e.offsetX, e.offsetY, 10, 0, 2 * Math.PI, false);
+    // ctx.fillStyle = 'green';
+    // ctx.fill();
 }
 
 // function clearCanvas() {
@@ -34,9 +38,10 @@ canvas.addEventListener('mousemove', draw)
 canvas.addEventListener('mousedown', (e) => {
     isDrawing = true;
     [lastX, lastY] = [e.offsetX, e.offsetY];
-    // lastX = e.offsetX
-    // lastY = e.offsetY
     ctx.moveTo(lastX, lastY);
+    ctx.lineTo(e.offsetX, e.offsetY);
 })
-canvas.addEventListener('mouseup', () => isDrawing = false)
+canvas.addEventListener('mouseup', () => {
+    isDrawing = false;
+})
 canvas.addEventListener('mouseout', () => isDrawing = false)
