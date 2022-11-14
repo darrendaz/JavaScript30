50a3af56fef0b156b5449481fa144c1069b432c3
Darren Lou <darren.lou@gmail.com>
2022-11-13 16:42:14 -0800

    draw works with destructuring assignment

--- 08 - Fun with HTML5 Canvas/index.js
+++ 08 - Fun with HTML5 Canvas/index.js
@@ -12,23 +12,31 @@ let lastX = 0
 let lastY = 0
 
 function draw(e) {
-    if(!isDrawing) return
+    if (!isDrawing) return;
 
     ctx.beginPath();
     // ctx.arc(e.clientX, e.clientY, 10, 0, 2 * Math.PI, false);
     // ctx.fillStyle = 'orange';
     // ctx.fill();
-
-    ctx.moveTo(lastX, lastY)
-    ctx.lineTo(e.offset, e.offsetY)
-    ctx.stroke()
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+    // lastX = e.offsetX 
+    // lastY = e.offsetY
 }
 
-function clearCanvas() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-}
+// function clearCanvas() {
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+// }
 
 canvas.addEventListener('mousemove', draw)
-canvas.addEventListener('mousedown', () => isDrawing = true)
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+    // lastX = e.offsetX
+    // lastY = e.offsetY
+    ctx.moveTo(lastX, lastY);
+})
 canvas.addEventListener('mouseup', () => isDrawing = false)
 canvas.addEventListener('mouseout', () => isDrawing = false)
