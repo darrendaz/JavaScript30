0d31b691a9de3cd52454b32562c4618317b2784d
Darren L <darren.lou@gmail.com>
2022-11-15 19:18:43 -0800

    checking in

--- 10 - Hold Shift and Check Checkboxes/index.js
+++ 10 - Hold Shift and Check Checkboxes/index.js
@@ -1,20 +1,42 @@
-const items = document.querySelectorAll('.item')
+const checkboxes = document.querySelectorAll('input[type="checkbox"]')
+const checkboxesArray = Array.from(checkboxes)
 let shiftKeyDown = false
 
-items.forEach(item => {
-    item.addEventListener('click', (e) => console.log(e))
-    if (shiftKeyDown = true) {
-        // grab the index of the nearest previous checked element and the current element
-        // use the range to apply checked state to all items with indexes within the range
-    }
+// Add event listeners to each checkbox
+checkboxes.forEach((checkbox, i) => {
+    checkbox.addEventListener('click', (e) => {
+
+        if (shiftKeyDown && e.currentTarget.checked) {
+            console.log(i)
+        }
+
+        let checked = checkboxesArray.filter((checkbox, i) => checkbox.checked === true)
+
+        // console.log(checked);
+
+    })
+
+    // if checkbox.check
+
+    // let something = item
+
+    //     // use the range to apply checked state to all items with indexes within the range
+    // }
 })
+
+
+// on click checkbox, check if there is a previous
 
 window.addEventListener('keydown', (e) => {
     shiftKeyDown = !!(e.key.toLowerCase() == 'shift')
-    console.log(shiftKeyDown);
 })
 
 window.addEventListener('keyup', (e) => {
     shiftKeyDown = !(e.key.toLowerCase() == 'shift')
-    console.log(shiftKeyDown);
 })
+
+function selectMultiple (shiftKeyDown) {
+    shiftKeyDown === false ? '' : () => {
+        console.log(shiftKeyDown);
+    }
+}
