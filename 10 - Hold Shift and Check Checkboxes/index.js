d2d53555d3741cbc900ec2d90cbac67c20d4bd88
Darren Lou <darren.lou@gmail.com>
2022-11-17 15:03:16 -0800

    complete

--- 10 - Hold Shift and Check Checkboxes/index.js
+++ 10 - Hold Shift and Check Checkboxes/index.js
@@ -1,42 +1,23 @@
-const checkboxes = document.querySelectorAll('input[type="checkbox"]')
-const checkboxesArray = Array.from(checkboxes)
-let shiftKeyDown = false
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]')
+let lastChecked
 
-// Add event listeners to each checkbox
-checkboxes.forEach((checkbox, i) => {
-    checkbox.addEventListener('click', (e) => {
+function handleCheck(e){
+    let inBetween = false
 
-        if (shiftKeyDown && e.currentTarget.checked) {
-            console.log(i)
-        }
-
-        let checked = checkboxesArray.filter((checkbox, i) => checkbox.checked === true)
-
-        // console.log(checked);
-
-    })
-
-    // if checkbox.check
-
-    // let something = item
-
-    //     // use the range to apply checked state to all items with indexes within the range
-    // }
-})
-
-
-// on click checkbox, check if there is a previous
-
-window.addEventListener('keydown', (e) => {
-    shiftKeyDown = !!(e.key.toLowerCase() == 'shift')
-})
-
-window.addEventListener('keyup', (e) => {
-    shiftKeyDown = !(e.key.toLowerCase() == 'shift')
-})
-
-function selectMultiple (shiftKeyDown) {
-    shiftKeyDown === false ? '' : () => {
-        console.log(shiftKeyDown);
+    if (e.shiftKey && this.checked) {
+        checkboxes.forEach((checkbox) => {
+            if (checkbox === this || checkbox === lastChecked){
+                inBetween = !inBetween
+            }
+            if (inBetween){
+                checkbox.checked = true
+            }
+        })
     }
+    
+    lastChecked = this
 }
+
+checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener('click', handleCheck)
+})
