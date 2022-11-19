b6e7ad8952224f34aefbb3392bc4675a4dff81a1
Darren Lou <darren.lou@gmail.com>
2022-11-18 16:15:23 -0800

    handle rewind and handleFF

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,33 +1,61 @@
-const video = document.querySelector('.player__controls player__video, .viewer')
-const playButton = document.querySelector('.player__controls .player__button, .toggle')
-const progressBar = document.querySelector('.player__controls .progress')
-const progressFill = document.querySelector('.player__controls .progress__filled')
-const volumeControls = document.querySelector('.player__controls input[name="volume"]');
-const playbackRateControls = document.querySelector('.player__controls input[name="playbackRate"]');
+const video = document.querySelector(
+    '.player__controls player__video, .viewer')
+const playButton = document.querySelector(
+    '.player__controls .player__button, .toggle')
+const progress = document.querySelector(
+    '.player__controls .progress')
+const progressBar = document.querySelector(
+    '.player__controls .progress__filled')
+const volumeControls = document.querySelector(
+    '.player__controls input[name="volume"]');
+const playbackRateControls = document.querySelector(
+    '.player__controls input[name="playbackRate"]');
+const rewindButton = document.querySelector(
+    '.player__controls [data-skip="-10"]')
+const fastforwardButton = document.querySelector(
+    '.player__controls [data-skip="25"]')
 
-let playing = false
 let mouseDown
 let originX = 0
 
 window.addEventListener('load', handlePageLoad);
-video.addEventListener('click', handleClickVideo)
-progressBar.addEventListener('mousedown', handleMouseDownProgressBar)
-window.addEventListener('mousedown', handleMouseDown)
-window.addEventListener('mouseup', handleMouseUp)
-window.addEventListener('mousemove', handleMouseMove)
+video.addEventListener('click', handleClickPlay);
+playButton.addEventListener('click', handleClickPlay);
+rewindButton.addEventListener('click', handleRewind);
+fastforwardButton.addEventListener('click', handleFastforward);
+progress.addEventListener('mousedown', handleMouseDownProgress);
+// window.addEventListener('mousedown', handleMouseDown)
+// window.addEventListener('mouseup', handleMouseUp)
+// window.addEventListener('mousemove', handleMouseMove)
 
 
-function handleClickVideo() {
+function handleClickPlay() {
     
-    !playing ? video.play() : video.pause()
-    playing = !playing
+    video.paused ? video.play() : video.pause()
+    video.paused ? playButton.innerHTML = '►' : playButton.innerHTML = '🁢 🁢' 
+}
+
+function handleRewind() {
+    let rewindTime = this.dataset.skip
+    video.currentTime >= Math.abs(rewindTime) ? 
+        video.currentTime += parseFloat(rewindTime) : 
+        video.currentTime = parseFloat(0)
+}
+
+function handleFastforward() {
+    console.log('fast forwarding')
+    let forwardTime = this.dataset.skip
+    video.duration <= Math.abs(forwardTime + video.currentTime) ? 
+        video.currentTime += parseFloat(forwardTime) : 
+        video.currentTime = video.duration
+    console.log(video.currentTime)
 }
 
 function handlePageLoad() {
-    video.currentTime = 0
+    video.currentTime = 30
 }
 
-function handleMouseDownProgressBar(e) {
+function handleMouseDownProgress(e) {
     originX = e.offsetX
     e.target.style.background = '#ffffff'
 }
