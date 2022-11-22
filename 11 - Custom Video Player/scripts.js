020ab29577bebbe3faa73a81892dea5d15a298e4
Darren L <darren.lou@gmail.com>
2022-11-22 10:26:58 -0800

    completed video player

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,15 +1,12 @@
 const video = document.querySelector('.player__controls player__video, .viewer')
 const playButton = document.querySelector('.player__controls .player__button, .toggle')
-const progress = document.querySelector('.player__controls .progress')
+const progressContainer = document.querySelector('.player__controls .progress')
 const progressBar = document.querySelector('.player__controls .progress__filled')
 const playbackRateToggle = document.querySelector('.player__button.playback__rate');
 const playbackRateControls = document.querySelector('.player__controls input[name="playbackRate"]');
 const volumeControls = document.querySelector('.player__controls input[name="volume"]');
 const rewindButton = document.querySelector('.player__controls [data-skip="-10"]')
 const fastforwardButton = document.querySelector('.player__controls [data-skip="25"]')
-
-let mouseDown
-let originX = 0
 
 window.addEventListener('load', handlePageLoad);
 
@@ -22,16 +19,25 @@ volumeControls.addEventListener('input', handleChangeVolume)
 playbackRateControls.addEventListener('input', handleChangePlaybackRate)
 playbackRateToggle.addEventListener('click', handleClickPlaybackRateToggle)
 
-progress.addEventListener('mousedown', handleMouseDownProgress);
+progressContainer.addEventListener('click', scrub);
+progressContainer.addEventListener('mousedown', () => mousedown = true);
+progressContainer.addEventListener('mouseup', () => mousedown = false );
+progressContainer.addEventListener('mousemove', (e) => mousedown && scrub(e))
+
+let mousedown = false
+let originX = 0
 
 
-// window.addEventListener('mousedown', handleMouseDown)
-// window.addEventListener('mouseup', handleMouseUp)
-// window.addEventListener('mousemove', handleMouseMove)
+function scrub(e) {
+    const scrubTime = (e.offsetX / progressContainer.offsetWidth) * video.duration
+    console.log('scrub time: ', scrubTime)
+    video.currentTime = scrubTime
+}
 
 function updateProgressBar() {
-    let percentage = Math.ceil((parseFloat(this.currentTime) / this.duration) * 1000)
+    let percentage = video.currentTime / video.duration * 100
     console.log(percentage); 
+    progressBar.style.flexBasis = `${percentage}%`
 }
 
 function handleChangeVolume() {
@@ -44,14 +50,16 @@ function handleChangePlaybackRate() {
 }
 
 function handleClickPlaybackRateToggle() {
-    if(video.playbackRate < parseFloat(playbackRateControls.max)){
+    if(video.playbackRate <= parseFloat(playbackRateControls.max)){
         video.playbackRate += parseFloat(playbackRateControls.step);
         playbackRateToggle.innerHTML = `${video.playbackRate}x`;
         playbackRateControls.value = `${video.playbackRate}`
     }
-
-    if(video.playbackRate === parseFloat(playbackRateControls.max)) {
+    
+    if(video.playbackRate > parseFloat(playbackRateControls.max)) {
         video.playbackRate = parseFloat(playbackRateControls.min);
+        playbackRateToggle.innerHTML = `${video.playbackRate}x`;
+        playbackRateControls.value = `${video.playbackRate}`
     }
 }
 
@@ -63,46 +71,17 @@ function handleClickPlay() {
 function handleRewind() {
     let rewindTime = this.dataset.skip
     video.currentTime >= Math.abs(rewindTime) ? 
-        video.currentTime += parseFloat(rewindTime) : 
+    video.currentTime += parseFloat(rewindTime) : 
         video.currentTime = parseFloat(0)
 }
-
+    
 function handleFastforward() {
     let forwardTime = this.dataset.skip
     video.duration > Math.abs(video.currentTime) + forwardTime ? 
-        video.currentTime += parseFloat(forwardTime) : 
-        video.currentTime = video.duration
+    video.currentTime += parseFloat(forwardTime) : 
+    video.currentTime = video.duration
 }
-
+    
 function handlePageLoad() {
     video.currentTime = 0
-}
-
-function handleMouseDownProgress(e) {
-    originX = e.offsetX
-    e.target.style.background = '#ffffff'
-}
-
-function handleMouseDown(e) {
-    mouseDown = true
-    // console.log(e.target)
-    // if (e.target === progressBar){
-        
-    // }
-}
-
-function handleMouseUp(e){
-    mouseDown = false
-    let change = e.offsetX - originX
-}
-
-function handleMouseMove(e) {
-    if (mouseDown) {
-        // console.log(e.offsetX, e.offsetY)
-        // console.log(progressFill)
-        // console.log(progressBar)
-        // console.log(volumeControls)
-        // console.log(playbackRateControls)
-    }
-    
 }
