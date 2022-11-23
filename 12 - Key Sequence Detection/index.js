66b4deafab84e34badebc56bbf82269ed22ab1e1
Darren Lou <darren.lou@gmail.com>
2022-11-22 23:06:35 -0800

    complete w konami code

--- 12 - Key Sequence Detection/index.js
+++ 12 - Key Sequence Detection/index.js
@@ -1,3 +1,15 @@
 window.addEventListener('keyup', (e) => {
-    console.log(e.key)
+    keysPressed(e.key)
 })
+
+const pressed = []
+const secretCode = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a']
+
+function keysPressed (key) {
+    pressed.push(key)
+    pressed.splice(-pressed.length - 1, pressed.length - secretCode.length)
+    if (pressed.join('') === secretCode.join('')){
+        cornify_add();
+        console.log(pressed.join(''), secretCode.join(''));
+    }
+}
