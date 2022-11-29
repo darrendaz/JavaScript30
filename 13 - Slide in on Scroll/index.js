d945b7b02c87e322186388b08ef7c2e542558cc0
Darren Lou <darren.lou@gmail.com>
2022-11-28 18:51:15 -0800

    completed

--- 13 - Slide in on Scroll/index.js
+++ 13 - Slide in on Scroll/index.js
@@ -1,21 +1,6 @@
-// function debounce(func, wait = 20, immediate = true) {
-//     var timeout;
-//     return function() {
-//       var context = this, args = arguments;
-//       var later = function() {
-//         timeout = null;
-//         if (!immediate) func.apply(context, args);
-//       };
-//       var callNow = immediate && !timeout;
-//       clearTimeout(timeout);
-//       timeout = setTimeout(later, wait);
-//       if (callNow) func.apply(context, args);
-//     };
-//   }
 function debounce(func, timeout = 50) {
   let timer;
   return (...args) => {
-
     clearTimeout(timer);
     timer = setTimeout(() => {
         func.apply(this, args)
@@ -23,11 +8,21 @@ function debounce(func, timeout = 50) {
   }
 }
 
+const sliderImages = document.querySelectorAll('.slide-in')
 
-const sliderImages = document.querySelector('.slide-in')
+window.addEventListener('scroll', debounce(checkSlide))
 
-window.addEventListener('scroll', debounce(checkSlide, 150))
+function checkSlide(){
+  sliderImages.forEach((image) => {
+    const slideInAt = (window.scrollY + window.innerHeight) - (image.height / 2)
+    const imageBottom = image.offsetTop + image.height
+    const isHalfShown = slideInAt > image.offsetTop
+    const notScrolledPast = window.scrollY < imageBottom
 
-function checkSlide(e){
-  console.log(e)
+    if (isHalfShown && notScrolledPast){
+      image.classList.add('active')
+    } else{
+      image.classList.remove('active')
+    }
+  });
 }
