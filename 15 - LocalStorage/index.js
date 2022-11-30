58c76abb042e03b600920e94561fb59441ef4b20
Darren Lou <darren.lou@gmail.com>
2022-11-29 19:26:53 -0800

    WIP addItme

--- 15 - LocalStorage/index.js
+++ 15 - LocalStorage/index.js
@@ -1,3 +1,14 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 const items = [];
+
+addItems.addEventListener('submit', addItem)
+
+function addItem(e) {
+    e.preventDefault();
+    const text = this.querySelector('[name=item]')
+    const item = {
+        text: 'Item Name',
+        done: false
+    }
+}
