90b76e0d5d873fa53893d5ad719b8f9d43b7d7f5
Darren L <darren.lou@gmail.com>
2022-12-01 14:05:30 -0800

    complete

--- 14 - JavaScript References VS Copying/index.js
+++ 14 - JavaScript References VS Copying/index.js
@@ -1,26 +1,26 @@
 // start with strings, numbers and booleans
-let age = 100
-let age2 = age
+let age = 100;
+let age2 = age;
 console.log(age, age2);
-age = 200
+age = 200;
 console.log(age, age2);
 
-let name = 'Darren'
-let name2 = name
+let name = "Darren";
+let name2 = name;
 console.log(name, name2);
-name = 'Rudy'
-console.log(name, name2)
+name = "Rudy";
+console.log(name, name2);
 
 // Let's say we have an array
-const players = ['Darren', 'Reggie', 'Isaac', 'Jerry'];
+const players = ["Darren", "Reggie", "Isaac", "Jerry"];
 
 // and we want to make a copy of it.
 const team = players;
 
-console.log(players, team)
+console.log(players, team);
 
 // You might think we can just do something like this:
-team[3] = 'Manny'
+team[3] = "Manny";
 
 // however what happens when we update that array?
 
@@ -33,22 +33,22 @@ team[3] = 'Manny'
 // So, how do we fix this? We take a copy instead!
 
 // one way
-const team2 = players.slice()
+const team2 = players.slice();
 
 // or create a new array and concat the old one in
-const team4 = [].concat(players)
+const team4 = [].concat(players);
 
 // or use the new ES6 Spread
-const team3 = ['Before spread', ...players, 'OH WOW']
+const team3 = ["Before spread", ...players, "after spread"];
 
-team4[3] = 'LeBron'
-team4.splice(0, 0, 'first')
+team4[3] = "LeBron";
+team4.splice(0, 0, "first");
 
-console.log(team2, team3, team4)
+console.log(team2, team3, team4);
 
-const team5 = Array.from(players)
-team5[3] = 'Luka'
-console.log(team5)
+const team5 = Array.from(players);
+team5[3] = "Luka";
+console.log(team5);
 
 // now when we update it, the original one isn't changed
 
@@ -56,33 +56,33 @@ console.log(team5)
 
 // with Objects
 const person = {
-    name: 'Charlie Brown',
-    age: 80
+  name: "Charlie Brown",
+  age: 80,
 };
 
 // and think we make a copy:
 const captain = person;
-captain.number = 99
+captain.number = 99;
 
 // how do we take a copy instead?
-const cap2 = Object.assign({}, person, { number: 99})
-console.log(cap2)
+const cap2 = Object.assign({}, person, { number: 99 });
+console.log(cap2);
 
 // using ...spread
 
-const cap3 = {...person}
+const cap3 = { ...person };
 
 const dar = {
-    name: 'darren',
-    age: 100,
-    social: {
-        twitter: '@darrendaz',
-        facebook: 'darren.l',
-        object: {id: 1, task: 'update list'}
-    }
-}
+  name: "darren",
+  age: 100,
+  social: {
+    twitter: "@darrendaz",
+    facebook: "darren.l",
+    object: { id: 1, task: "update list" },
+  },
+};
 
-console.log(dar)
-const dev = Object.assign({}, {...dar, age:2000})
+console.log(dar);
+const dev = Object.assign({}, { ...dar, age: 2000 });
 
-const dev2 = JSON.parse(JSON.stringify(dar))
+const dev2 = JSON.parse(JSON.stringify(dar));
