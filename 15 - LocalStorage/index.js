f73fdb461d6a9bcaad2e680d897c00dbd768754e
Darren L <darren.lou@gmail.com>
2022-12-01 14:00:44 -0800

    complete

--- 15 - LocalStorage/index.js
+++ 15 - LocalStorage/index.js
@@ -1,18 +1,50 @@
-const addItems = document.querySelector('.add-items');
-const itemsList = document.querySelector('.plates');
-const items = [];
-
-addItems.addEventListener('submit', addItem)
+const addItems = document.querySelector(".add-items");
+const itemsList = document.querySelector(".plates");
+const items = JSON.parse(localStorage.getItem("items")) || [];
 
 function addItem(e) {
-    e.preventDefault();
-    const text = (this.querySelector('[name=item]')).value
-    const item = {
-        text: 'Item Name',
-        done: false
-    }
+  e.preventDefault();
+  const text = this.querySelector("[name=item]").value;
+  const item = {
+    text,
+    done: false,
+  };
 
-    items.push(item)
-    this.reset()
-
+  items.push(item);
+  populateList(items, itemsList);
+  localStorage.setItem("items", JSON.stringify(items));
+  this.reset();
 }
+
+function populateList(plates = [], platesList) {
+  platesList.innerHTML = plates
+    .map((plate, i) => {
+      return `
+        <li>
+        <input type="checkbox" data-index=${i} id="item${i}" ${
+        plate.done ? "checked" : ""
+      }/>
+        <label for="item${i}">${plate.text}</label>
+        </li>
+        `;
+    })
+    .join("");
+}
+
+function toggleDone(e) {
+  if (!e.target.matches("input")) return;
+  const el = e.target;
+  const index = el.dataset.index;
+  items[index].done = !items[index].done;
+  localStorage.setItem("items", JSON.stringify(items));
+  populateList(items, itemsList);
+}
+
+// Add check all button and function
+
+// Add reset button and function
+
+addItems.addEventListener("submit", addItem);
+itemsList.addEventListener("click", toggleDone);
+
+populateList(items, itemsList);
