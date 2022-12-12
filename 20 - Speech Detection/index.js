c98f7a06965185b8b510ac77492a4fb562346f9e
Darren L <darren.lou@gmail.com>
2022-12-12 10:04:59 -0800

    complete

--- 20 - Speech Detection/index.js
+++ 20 - Speech Detection/index.js
@@ -9,7 +9,6 @@ const words = document.querySelector('.words');
 words.appendChild(p);
 
 recognition.addEventListener('result', (e) => {
-  console.log(e.results);
   const transcript = Array.from(e.results)
     .map((result) => result[0])
     .map((result) => result.transcript)
