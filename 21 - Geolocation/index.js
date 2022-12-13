371e5ae99f3cb14dbe72934c4680ace4a40f507b
Darren Lou <darren.lou@gmail.com>
2022-12-12 18:59:06 -0800

    complete

--- 21 - Geolocation/index.js
+++ 21 - Geolocation/index.js
@@ -1,6 +1,21 @@
 const arrow = document.querySelector('.arrow');
 const speed = document.querySelector('.speed-value');
 
-navigator.geolocation.watchPosition((data) => {
-  console.log(data);
-});
+function getMiles(meters) {
+  return meters * 0.000621371192;
+}
+function getMeters(miles) {
+  return miles * 1609.344;
+}
+navigator.geolocation.watchPosition(
+  (data) => {
+    speed.textContent = (Math.floor(data.coords.speed * 2.23694) * 10) / 10;
+    arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+  },
+  (err) => {
+    console.err(err);
+    alert(
+      'Something went wrong. Please make sure you allowed location permissions'
+    );
+  }
+);
