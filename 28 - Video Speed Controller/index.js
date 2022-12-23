e8689cc0c696482e6e124ff1d56d98f392946f80
Darren L <darren.lou@gmail.com>
2022-12-23 12:18:36 -0800

    complete

--- 28 - Video Speed Controller/index.js
+++ 28 - Video Speed Controller/index.js
@@ -2,14 +2,17 @@ const speed = document.querySelector('.speed');
 const bar = speed.querySelector('.speed-bar');
 const video = document.querySelector('.flex');
 
-speed.addEventListener('mousemove', (e) => {
+speed.addEventListener('mousemove', handleMove);
+
+function handleMove(e) {
   const y = e.pageY - this.offsetTop;
-  const percent = y / this.offsetWeight;
+  const percent = y / this.offsetHeight;
   const min = 0.4;
   const max = 4;
   const height = Math.round(percent * 100) + '%';
   const playbackRate = percent * (max - min) + min;
-
+  console.log(playbackRate);
   bar.style.height = height;
-  console.log(percent);
-});
+  bar.textContent = playbackRate.toFixed(2) + 'x';
+  video.playbackRate = playbackRate;
+}
